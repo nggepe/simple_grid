1f92f882812fa17441a4a74a553cf02a799406e6
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2021-03-11 10:34:48 +0700

    add an example

---
+++ example/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:simple_grid/simple_grid.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple_grid',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple_grid"),
+      ),
+      body: SpGrid(width: MediaQuery.of(context).size.width, children: [
+        SpGridItem(
+          xs: 12,
+          sm: 6,
+          md: 4,
+          lg: 3,
+          child: Container(
+            color: Colors.blue,
+            height: 200,
+          ),
+        ),
+        SpGridItem(
+          xs: 12,
+          sm: 6,
+          md: 4,
+          lg: 3,
+          child: Container(
+            color: Colors.green,
+            height: 200,
+          ),
+        ),
+        SpGridItem(
+          xs: 12,
+          sm: 6,
+          md: 4,
+          lg: 3,
+          child: Container(
+            color: Colors.yellow,
+            height: 200,
+          ),
+        ),
+        SpGridItem(
+          xs: 12,
+          sm: 6,
+          md: 4,
+          lg: 3,
+          child: Container(
+            color: Colors.purple,
+            height: 200,
+          ),
+        ),
+      ]),
+    );
+  }
+}
